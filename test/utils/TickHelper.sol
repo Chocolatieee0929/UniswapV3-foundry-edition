ac5bc54c50cf4d656989e557517586d65838e765
Chocolatieee0929 <luoruixx@163.com>
2024-02-21 13:51:01 +0800

    add: the function price-to-tick

--- test/utils/TickHelper.sol
+++ test/utils/TickHelper.sol
@@ -10,10 +10,19 @@ int24 constant TICK_LOW = 10;
 int24 constant TICK_MEDIUM = 60;
 int24 constant TICK_HIGH = 200;
 
+// mapping(uint24 => int24)  feeAmountTickSpacing;
+// feeAmountTickSpacing[500] = 10;
+//         feeAmountTickSpacing[3000] = 60;
+//         feeAmountTickSpacing[10000] = 200;
+
 function getMinTick(int24 tickSpacing) pure returns (int24) {
-    return (TickMath.MIN_TICK / tickSpacing) * tickSpacing;
+	return (TickMath.MIN_TICK / tickSpacing) * tickSpacing;
 }
 
 function getMaxTick(int24 tickSpacing) pure returns (int24) {
-    return (TickMath.MAX_TICK / tickSpacing) * tickSpacing;
+	return (TickMath.MAX_TICK / tickSpacing) * tickSpacing;
+}
+
+function getTick(uint160 sqrtPriceX96) pure returns (int24) {
+	return TickMath.getTickAtSqrtRatio(sqrtPriceX96);
 }
